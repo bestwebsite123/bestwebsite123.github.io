39b6055e713c00e135b0f53d57ea4e08d17ce6bb
bestwebsite123 <142743294+bestwebsite123@users.noreply.github.com>
2023-09-10 14:16:52 -0400

    hi

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
 const link1= document.getElementById("topic1");
 const link2= document.getElementById("topic2");
 const link3= document.getElementById("topic3");
-const link4= document.getElementById("info");
+const link4= document.getElementById("topic4");
+
+const info = document.getElementById("info");
+
+AOS.init();
 
 link1.addEventListener("click",function() {
     info.innerHTML = `
@@ -22,4 +26,11 @@ link3.addEventListener("click",function() {
     <h1>Famous Dogs</h1>
     <p>On many social platforms like youtube, tiktok, instagram, snapchat, and more there are many "famous" dogs. Like Tucker Budzyn, Cedrick the chihuahua, Brodie the Dood, and many more. Many peoples dogs get famous because they do funny stuff or even dance.</p>
     `
+});
+
+link4.addEventListener("click",function() {
+    info.innerHTML = `
+    <h1>Dogs</h1>
+    <img src="https://pngimg.com/uploads/dog/dog_PNG50292.png" height=200> </img>
+    <iframe width="300" height="205" src="https://www.youtube.com/embed/LlG9L8GQ1Gg?si=mtY-tZlB2NQs03HP" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>    `
 });
