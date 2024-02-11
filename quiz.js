333bb3197b2fa985c2dfd6b79aff5ff2e5d6549d
bestwebsite123 <142743294+bestwebsite123@users.noreply.github.com>
2024-02-11 13:57:49 -0500

    hi

---
+++ quiz.js
@@ -0,0 +1,21 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const submitBtn = document.getElementById('submit-btn');
+    const resultDiv = document.getElementById('result');
+
+    submitBtn.addEventListener('click', function(event) {
+        event.preventDefault();
+        let score = 0;
+        const correctAnswers = ['bob', 'bita'];
+        
+        correctAnswers.forEach((answer, index) => {
+            const question = document.querySelector(`select[name="question${index + 1}"]`);
+            if (question && question.value === answer) {
+                score++;
+            }
+        }
+        )
+
+resultDiv.innerHTML = `Your score is ${score} out of 10.`;
+
+    });
+});
